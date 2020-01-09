0efa502d8af23972937b214683baa463b83b16e0
huanghua <yccai_hh@aliyun.com>
2020-01-09 14:29:18 +0800

    集成knife4j

---
+++ src/main/java/com/example/demo/Helloboot.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Helloboot {
+    @RequestMapping("/h")
+    public String abc(){
+        return "你好，你已经搭好了Spring Boot";
+    }
+}
