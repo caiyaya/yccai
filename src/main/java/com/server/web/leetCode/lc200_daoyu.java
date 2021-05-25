64222823aa2c215de2f785ac09f08ccb09b34993
huanghua <huanghua@fadada.com>
2021-05-25 14:00:51 +0800

    lc

--- src/main/java/com/server/web/leetCode/lc200_daoyu.java
+++ src/main/java/com/server/web/leetCode/lc200_daoyu.java
@@ -28,15 +28,21 @@ class lc200_daoyu {
      * 输出：1
      */
     public static int numIsLands(char[][] grid) {
-        //岛屿数量
+        // 统计岛屿的个数
         int count = 0;
         if (grid == null || grid.length == 0) {
             return 0;
         }
+        // 两个for循环遍历每一个格子
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
+                // 只有当前格子是1才开始计算
                 if (grid[i][j] == '1') {
+                    // 然后通过bfs把当前格子的上下左右4
+                    // 个位置为1的都要置为0，因为他们是连着
+                    // 一起的算一个岛屿，
                     dfs(grid, i, j);
+                    // 如果当前格子是1，岛屿的数量加1
                     count++;
                 }
             }
@@ -46,6 +52,7 @@ class lc200_daoyu {
 
     /**
      * 深度优先遍历
+     *
      * @param grid
      * @param i
      * @param j
